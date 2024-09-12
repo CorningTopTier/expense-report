386e9d898615105c3b977d19c0ca57f5dd630651
Jake Allen <corningtoptier@gmail.com>
2024-09-12 10:27:40 -0400

    test: updated our tests to use assertpy

--- test_expense_report.py
+++ test_expense_report.py
@@ -1,9 +1,12 @@
 import unittest
+from assertpy import assert_that
+
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        # self.assertEqual(True, False)  # add assertion here
+        assert_that(True, "some value").is_equal_to(False).is_false()
 
 
 if __name__ == '__main__':
