a52307df2f3e38a744dafe60a7995fa30035c180
Jake Allen <corningtoptier@gmail.com>
2024-09-19 10:13:03 -0400

    test: added a test test_should_output_header_followed_by_expense_data_with_expense_report_with_one_expense

--- expense_report.py
+++ expense_report.py
@@ -1,3 +1,6 @@
 class ExpenseReport:
     def generate_report(self):
         return "| Date | Description | Amount | Balance |"
+
+    def initialize(self, date, description, amount, balance):
+        pass
