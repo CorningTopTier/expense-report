91d8912d97178902665e28e44b67ee93398ab8b7
Jake Allen <corningtoptier@gmail.com>
2024-09-19 10:03:23 -0400

    test: commented out test test_should_output_header_followed_by_expense_data_with_expense_report_with_one_expense(self):

--- test_expense_report.py
+++ test_expense_report.py
@@ -17,6 +17,20 @@ class ExpenseReportTests(unittest.TestCase):
         # Then
         assert_that(expense_report_output, "export report").is_equal_to("| Date | Description | Amount | Balance |")
 
+    # def test_should_output_header_followed_by_expense_data_with_expense_report_with_one_expense(self):
+    #     # Given
+    #         # an expense report without any expense
+    #     expense_report = ExpenseReport(date="6/05/1998", description="Gasoline", amount=52.25, balance=1586.24)  # an expense report without any expense
+    #
+    #     # When
+    #     expense_report_output = expense_report.generate_report()
+    #
+    #     # Then
+    #     (assert_that(expense_report_output, "export report")
+    #         .starts_with("| Date | Description | Amount | Balance |")
+    #         .ends_with("| 6/05/1998 | Gasoline | 52.25 | 1,586.24 |"))
+
+
 
 if __name__ == '__main__':
     unittest.main()
