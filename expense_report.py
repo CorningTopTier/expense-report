fea28bddd7f139e68bd365ff566de3b015634634
Jake Allen <corningtoptier@gmail.com>
2024-09-26 11:17:58 -0400

    refactor: replace individual fields w/ an expense object

--- expense_report.py
+++ expense_report.py
@@ -2,13 +2,11 @@ class ExpenseReport:
     def __init__(self):
         self.expense = None
         self.hasExpense = False
-        self.expenseDescription = None
-        self.expenseAmount = 0.00
-        self.expenseBalance = 0.00
+
     def generate_report(self):
         if self.hasExpense:
-            expenseLine = "| " + self.expense.date + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
-            expenseLine2 = "| " + self.expense.date + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
+            expenseLine = "| " + self.expense.date + " | " + self.expense.description + " | " + f"{self.expense.amount:.2f}" + " | " + f"{self.expense.balance:,.2f}" + " |"
+            expenseLine2 = "| " + self.expense.date + " | " + self.expense.description + " | " + f"{self.expense.amount:.2f}" + " | " + f"{self.expense.balance:,.2f}" + " |"
 
         else:
             expenseLine = ""
@@ -18,9 +16,6 @@ class ExpenseReport:
     def initialize(self, date, description, amount, balance):
         self.hasExpense = True
         self.expense = Expense(date, description, amount, balance)
-        self.expenseDescription = description
-        self.expenseAmount = amount
-        self.expenseBalance = balance
         pass
 
 class Expense:
