44b080ddcd15c6cd2b5f61c8970cba9ffeeb93ab
Jake Allen <corningtoptier@gmail.com>
2024-09-26 09:36:03 -0400

    feat: add support for amount

--- expense_report.py
+++ expense_report.py
@@ -1,11 +1,12 @@
 class ExpenseReport:
     def __init__(self):
+        self.expenseAmount = 0.00
         self.expenseDate = None
         self.expenseDescription = None
         self.hasExpense = False
     def generate_report(self):
         if self.hasExpense:
-            expenseLine = "| " + self.expenseDate + " | " + self.expenseDescription + " | 52.25 | 1,586.24 |"
+            expenseLine = "| " + self.expenseDate + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | 1,586.24 |"
         else:
             expenseLine = ""
         return "| Date | Description | Amount | Balance |" + expenseLine
@@ -14,4 +15,5 @@ class ExpenseReport:
         self.expenseDescription = description
         self.hasExpense = True
         self.expenseDate = date
+        self.expenseAmount = amount
         pass
