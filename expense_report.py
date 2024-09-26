6c445d3ff80c7c6c42ee63ec05b26ed5fdacffe8
Jake Allen <corningtoptier@gmail.com>
2024-09-26 11:07:02 -0400

    refactor: generate report using expense.expenseDate

--- expense_report.py
+++ expense_report.py
@@ -2,14 +2,13 @@ class ExpenseReport:
     def __init__(self):
         self.expense = None
         self.hasExpense = False
-        self.expenseDate = None
         self.expenseDescription = None
         self.expenseAmount = 0.00
         self.expenseBalance = 0.00
     def generate_report(self):
         if self.hasExpense:
-            expenseLine = "| " + self.expenseDate + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
-            expenseLine2 = "| " + self.expenseDate + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
+            expenseLine = "| " + self.expense.expenseDate + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
+            expenseLine2 = "| " + self.expense.expenseDate + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
 
         else:
             expenseLine = ""
@@ -19,7 +18,6 @@ class ExpenseReport:
     def initialize(self, date, description, amount, balance):
         self.hasExpense = True
         self.expense = Expense(date, description, amount, balance)
-        self.expenseDate = date
         self.expenseDescription = description
         self.expenseAmount = amount
         self.expenseBalance = balance
