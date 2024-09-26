cf7fc7d287c033aa2cb765e5bb12d507a78270f8
Jake Allen <corningtoptier@gmail.com>
2024-09-26 08:47:04 -0400

    test: added test test_should_output_header_followed_by_expense_data_with_expense_report_with_one_grocery_expense and test_should_output_header_followed_by_expense_data_with_expense_report_with_9_26_2024_expense

--- test_expense_report.py
+++ test_expense_report.py
@@ -18,7 +18,7 @@ class ExpenseReportTests(unittest.TestCase):
         # Then
         assert_that(expense_report_output, "export report").is_equal_to("| Date | Description | Amount | Balance |")
 
-    def test_should_output_header_followed_by_expense_data_with_expense_report_with_one_expense(self):
+    def test_should_output_header_followed_by_expense_data_with_expense_report_with_one_gasoline_expense(self):
         # Given
             # an expense report without any expense
         expense_report = ExpenseReport()  # an expense report without any expense
@@ -30,6 +30,33 @@ class ExpenseReportTests(unittest.TestCase):
         (assert_that(expense_report_output, "export report")
             .starts_with("| Date | Description | Amount | Balance |")
             .ends_with("| 6/05/1998 | Gasoline | 52.25 | 1,586.24 |"))
+
+    def test_should_output_header_followed_by_expense_data_with_expense_report_with_one_grocery_expense(self):
+        # Given
+            # an expense report without any expense
+        expense_report = ExpenseReport()  # an expense report without any expense
+        expense_report.initialize(date="6/05/1998", description="Groceries", amount=52.25, balance=1586.24)
+        # When
+        expense_report_output = expense_report.generate_report()
+
+        # Then
+        (assert_that(expense_report_output, "export report")
+            .starts_with("| Date | Description | Amount | Balance |")
+            .ends_with("| 6/05/1998 | Groceries | 52.25 | 1,586.24 |"))
+
+    def test_should_output_header_followed_by_expense_data_with_expense_report_with_9_26_2024_expense(self):
+        # Given
+            # an expense report without any expense
+        expense_report = ExpenseReport()  # an expense report without any expense
+        expense_report.initialize(date="09/26/2024", description="Groceries", amount=52.25, balance=1586.24)
+        # When
+        expense_report_output = expense_report.generate_report()
+
+        # Then
+        (assert_that(expense_report_output, "export report")
+            .starts_with("| Date | Description | Amount | Balance |")
+            .ends_with("| 09/26/2024 | Groceries | 52.25 | 1,586.24 |"))
+
 
 
 
