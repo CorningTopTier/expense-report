1cbc1889eda58799059bd470fcd87f1ff9fa68de
Jake Allen <corningtoptier@gmail.com>
2024-09-26 09:01:56 -0400

    feat: added support for variable expenseDate

--- expense_report.py
+++ expense_report.py
@@ -1,10 +1,11 @@
 class ExpenseReport:
     def __init__(self):
+        self.expenseDate = None
         self.expenseDescription = None
         self.hasExpense = False
     def generate_report(self):
         if self.hasExpense:
-            expenseLine = "| 6/05/1998 | " + self.expenseDescription + " | 52.25 | 1,586.24 |"
+            expenseLine = "| " + self.expenseDate + " | " + self.expenseDescription + " | 52.25 | 1,586.24 |"
         else:
             expenseLine = ""
         return "| Date | Description | Amount | Balance |" + expenseLine
@@ -12,4 +13,5 @@ class ExpenseReport:
     def initialize(self, date, description, amount, balance):
         self.expenseDescription = description
         self.hasExpense = True
+        self.expenseDate = date
         pass
