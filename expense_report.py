f56ea8656b96560fda903026e8a78f9078fc0b3d
Jake Allen <corningtoptier@gmail.com>
2024-09-26 11:01:20 -0400

    refactor: add a new expense during initialize()

--- expense_report.py
+++ expense_report.py
@@ -1,5 +1,6 @@
 class ExpenseReport:
     def __init__(self):
+        self.expense = None
         self.hasExpense = False
         self.expenseDate = None
         self.expenseDescription = None
@@ -8,14 +9,35 @@ class ExpenseReport:
     def generate_report(self):
         if self.hasExpense:
             expenseLine = "| " + self.expenseDate + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
+            expenseLine2 = "| " + self.expenseDate + " | " + self.expenseDescription + " | " + f"{self.expenseAmount:.2f}" + " | " + f"{self.expenseBalance:,.2f}" + " |"
+
         else:
             expenseLine = ""
-        return "| Date | Description | Amount | Balance |" + expenseLine
+            expenseLine2 = ""
+        return "| Date | Description | Amount | Balance |" + expenseLine + expenseLine2
 
     def initialize(self, date, description, amount, balance):
         self.hasExpense = True
+        self.expense = Expense()
         self.expenseDate = date
         self.expenseDescription = description
         self.expenseAmount = amount
         self.expenseBalance = balance
         pass
+
+class Expense:
+    def __init__(self):
+        self.expenseDate = None
+        self.expenseDescription = None
+        self.expenseAmount = 0.00
+        self.expenseBalance = 0.00
+
+
+
+
+
+
+
+
+
+
