7b3d30619e32873bba32ebce3345eaf593cb2172
Mike Kienenberger <mkienenb@gmail.com>
2024-10-17 11:19:50 -0400

    feat: calculate balance instead of using preset balance in expenses

--- expense_report.py
+++ expense_report.py
@@ -1,24 +1,34 @@
 class ExpenseReport:
     def __init__(self):
         self.expense_list = []
+        self.beginningBalance = 0
 
     def generate_report(self):
         report_so_far = "| Date | Description | Amount | Balance |"
+        currentBalance = self.beginningBalance
         for expense in self.expense_list:
-            expenseLine = self.createExpenseLine(expense)
+            currentBalance = currentBalance - expense.amount
+            expenseLine = self.createExpenseLine(expense, currentBalance)
             report_so_far = report_so_far + expenseLine
         return report_so_far
 
-    def createExpenseLine(self, expense):
+    def createExpenseLine(self, expense, currentBalance):
         if expense == None:
             expenseLine = ""
         else:
-            expenseLine = "| " + expense.date + " | " + expense.description + " | " + f"{expense.amount:.2f}" + " | " + f"{expense.balance:,.2f}" + " |"
+            if expense.balance is not None:
+                currentBalance = expense.balance
+
+            expenseLine = "| " + expense.date + " | " + expense.description + " | " + f"{expense.amount:.2f}" + " | " + f"{currentBalance:,.2f}" + " |"
         return expenseLine
 
-    def initialize(self, date, description, amount, balance):
+    def initialize(self, date, description, amount, balance=None):
         expense = Expense(date, description, amount, balance)
         self.expense_list.append(expense)
+
+
+    def setBeginningBalance(self, beginningBalance):
+        self.beginningBalance = beginningBalance
 
 
 class Expense:
@@ -27,14 +37,3 @@ class Expense:
         self.description = description
         self.amount = amount
         self.balance = balance
-
-
-
-
-
-
-
-
-
-
-
