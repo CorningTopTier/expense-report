a52ccbbf3f375fdc934ce30efb8a31fac9b0e498
Mike Kienenberger <mkienenb@gmail.com>
2024-11-14 10:45:36 -0500

    test: clean up spacing

--- test_expense_report_tk_gui.py
+++ test_expense_report_tk_gui.py
@@ -8,10 +8,8 @@ from expense_report_tk_gui import create_app
 class ExpenseReportTests(unittest.TestCase):
     def test_should_create_window_named_enter_expense(self):
         # Given
-
         # When
         root = create_app()
-
         # Then
         assert_that(root.title(), "window title").is_equal_to("Enter Expense")
 
